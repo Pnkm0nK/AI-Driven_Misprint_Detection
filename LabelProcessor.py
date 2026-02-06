7da70cc4548f76422bf46f393e6f129eb929e382
Mikita Zyhmantovich <nucklearduk0@gmail.com>
2026-02-06 11:46:07 +0100

    Add barcode accuracy metric for result storage + improve ROI image display

--- LabelProcessor.py
+++ LabelProcessor.py
@@ -1,12 +1,15 @@
 import cv2
-from metrics import calculate_character_error_rate
-from ImageProcessor import ImageProcessor, Type151ImageProcessor
-from ROIStorage import ROIStorage
-from ResultStorage import ResultStorage
+from matplotlib import image
 import numpy as np
 import os
 import pytesseract
 import dotenv
+import zxingcpp as zxing
+from PIL import Image, ImageDraw, ImageFont
+
+from ImageProcessor import ImageProcessor, Type151ImageProcessor
+from ROIStorage import ROIStorage
+from ResultStorage import ResultStorage
 
 class LabelProcessor:
     def __init__(self, label_scan_pdf_path: str,
@@ -17,7 +20,6 @@ class LabelProcessor:
 
         # get image from scan
         self.image_processor = image_processor
-        self.region_images: dict[str, np.ndarray] = {}
         self.full_label_image = self.image_processor.convert_pdf_to_image(label_scan_pdf_path)
 
         # get ROIs and establish starting position anchor point
@@ -30,9 +32,18 @@ class LabelProcessor:
 
         self.roi_coordinates = self.roi_storage.load_roi_json_data()
 
-        self.region_images: dict[str, np.ndarray] = self._extract_region_images()
-
+        self.text_region_images: dict[str, np.ndarray] = self._extract_region_images(self.roi_coordinates["text_regions"])
         self.region_texts: dict[str, str] = self._extract_all_region_texts()
+
+        self.barcode_images: dict[str, np.ndarray] = self._extract_region_images(self.roi_coordinates["barcode_regions"])
+        self.region_barcodes: dict[str, str] = self._extract_all_barcodes()
+
+    def _extract_roi(self, roi_name: str, coordinates: tuple[int, int, int, int]) -> np.ndarray:
+        x0, y0, x1, y1 = coordinates
+        image = self.full_label_image[y0:y1, x0:x1]
+        preprocess_method = self.image_processor.get_suitable_preprocessing_method(roi_name)
+        preprocessed_image = preprocess_method(image)
+        return preprocessed_image
 
     def _extract_text_from_region_image(self, region_image: np.ndarray) -> str:
         # Perform OCR using pytesseract
@@ -40,52 +51,105 @@ class LabelProcessor:
         text = pytesseract.image_to_string(region_image, config=custom_config)
         return text.strip()
 
-
-    def _extract_region_images(self) -> dict[str, np.ndarray]:
+    def _extract_region_images(self, roi_coordinates) -> dict[str, np.ndarray]:
         region_images = {}
-        for roi_name,(x0, y0, x1, y1) in self.roi_coordinates.items():
-            image = self.full_label_image[y0:y1, x0:x1]
-            preprocess_method = self.image_processor.get_suitable_preprocessing_method(roi_name)
-            preprocessed_image = preprocess_method(image)
-            region_images[roi_name] = preprocessed_image
+        for roi_name,(x0, y0, x1, y1) in roi_coordinates.items():
+            image = self._extract_roi(roi_name, (x0, y0, x1, y1))
+            region_images[roi_name] = image
         return region_images
-
+    
     def _extract_all_region_texts(self) -> dict[str, str]:
-        assert self.region_images, "Region images have not been extracted."
+        assert self.text_region_images, "Text region images have not been extracted."
 
         region_texts = {}
-        for roi_name, img in self.region_images.items():
+        for roi_name, img in self.text_region_images.items():
             region_texts[roi_name] = self._extract_text_from_region_image(img)
         return region_texts
     
     def get_extracted_texts(self) -> dict[str, str]:
         return self.region_texts
+    
+    def get_extracted_barcodes(self) -> dict[str, str]:
+        return self.region_barcodes
+    
+    def _extract_all_barcodes(self) -> dict[str, list]:
+        assert self.barcode_images, "Barcode images have not been extracted."
 
-    def display_region_images(self):
-        for roi_name, image in self.region_images.items():
+        barcode_results = {}
+        for roi_name, img in self.barcode_images.items():
+            barcodes = zxing.read_barcodes(img)
+            barcode_results[roi_name] = barcodes[0].text if barcodes else str("")
+        return barcode_results
+
+    @staticmethod 
+    def _display_region_image(roi_name: str, image: np.ndarray, result: str):
+            image = Image.fromarray(image)
+            image = image.resize((image.width * 2, image.height * 2), Image.Resampling.LANCZOS)
+            image = image.convert("RGB")
+
+            padding_y = 10
+            padding_x = 5
+            
+            display_text_region = f"Region: {roi_name}"
+            display_text_ocr_result = f"OCR Result: {result}"
+            
+            try:
+                font_title = ImageFont.truetype("AvenirNextWorld-Regular.ttf", 21)
+                font_text = ImageFont.truetype("AvenirNextWorld-Bold.ttf", 32)
+            except:
+                font_title = ImageFont.load_default()
+                font_text = ImageFont.load_default()
+            
+            # use text bboxes to expand canvas size if text doesn't fit
+            draw = ImageDraw.Draw(image)
+
+            title_bbox = draw.textbbox((0, 0), display_text_region, font=font_title)
+            text_bbox = draw.textbbox((0, 0), display_text_ocr_result, font=font_text)
+
+            title_width = title_bbox[2] - title_bbox[0]
+            title_height = title_bbox[3] - title_bbox[1]
+
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+
+            max_text_width = max(title_width, text_width)
+            
+            required_height = title_height + text_height + image.height + 4 * padding_y
+
+            required_width = max_text_width + padding_x
+            if required_width > image.width:
+                new_image = Image.new("RGB", (required_width, required_height), (255, 255, 255))
+                new_image.paste(image, (0, title_height + 2 * padding_y))
+            else:
+                new_image = Image.new("RGB", (image.width, required_height), (255, 255, 255))
+                new_image.paste(image, (0, title_height + 2 * padding_y))
+            
+            draw = ImageDraw.Draw(new_image)
+            draw.text((padding_x, padding_y), display_text_region, fill=(255, 100, 0), font=font_title)
+            draw.text((padding_x, image.height + title_height + 2 * padding_y), display_text_ocr_result, fill=(0, 100, 255), font=font_text)
+            
+            # Convert back to numpy for cv2.imshow
+            image_np = np.array(new_image)
+            cv2.imshow(roi_name, image_np)
+
+    def display_all_region_images(self):
+        for roi_name, image in self.text_region_images.items():
             if roi_name in self.region_texts:
-                image = image.copy()
-                image = cv2.resize(image, (image.shape[1]*2, image.shape[0]*2), interpolation=cv2.INTER_LINEAR)
-                image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-                image = cv2.copyMakeBorder(image, 50, 50, 0, 50, cv2.BORDER_CONSTANT, value=(255, 255, 255))
-                display_text_region = f"Region: {roi_name}"
-                display_text_ocr_result = f"OCR Result: {self.region_texts[roi_name]}"
-                cv2.putText(image, display_text_ocr_result, (5, image.shape[0]- 45),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 56, 255),2)
-                cv2.putText(image, display_text_region, (5, 35),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 56, 0),2)
-            cv2.imshow(roi_name, image)
+                self._display_region_image(roi_name, image, result=self.region_texts[roi_name])
+        for roi_name, image in self.barcode_images.items():
+            if roi_name in self.region_barcodes:
+                self._display_region_image(roi_name, image, result=self.region_barcodes[roi_name])
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     
 if __name__ == "__main__":
-    image_processor = Type151ImageProcessor()
-    processor = LabelProcessor("../label_scans/M333023W151.pdf",
-                               "./roi_data/label_151_rois.json",
+    image_processor = ImageProcessor()
+    processor = LabelProcessor("../label_scans/M333023W146.pdf",
+                               "./roi_data/label_146_rois.json",
                                 image_processor=image_processor)
-    result_storage = ResultStorage(processor.get_extracted_texts(), gt_texts_path="./ground_truth/label_151_texts_gt.json")
-    result_storage.add_cer_metric()
-    result_storage.add_metric_info_to_summary()
-    result_storage.add_extracted_texts_to_summary()
-    result_storage.save_extracted_texts_to_json("./results/label_151_texts.json")
-    result_storage.save_summary_to_txt("./results/label_151_summary.txt")
+    processor.display_all_region_images()
+    results = ResultStorage(processor.get_extracted_texts(),
+                            gt_texts_path="./ground_truth/label_146_texts_gt.json",
+                            extracted_barcodes=processor.get_extracted_barcodes()
+                            )
+    results.generate_summary(summary_name="label_146_barcode", output_dir="./results") 
--- ResultStorage.py
+++ ResultStorage.py
@@ -3,8 +3,11 @@ import json
 
 class ResultStorage:
 
-    def __init__(self, extracted_texts: dict[str, str], gt_texts_path: str = None):
+    def __init__(self, extracted_texts: dict[str, str],
+                gt_texts_path: str = None,
+                extracted_barcodes: dict[str, str]| None = None):
         self.extracted_texts = extracted_texts
+        self.extracted_barcodes = extracted_barcodes    
         try:
             with open(gt_texts_path, 'r', encoding='utf-8') as f:
                 gt_texts = json.load(f)
@@ -20,8 +23,10 @@ class ResultStorage:
             f.write(self.summary_text)
 
     def save_extracted_texts_to_json(self, output_json_path: str):
+        extracted_data = self.extracted_texts.copy()
+        extracted_data.update(self.extracted_barcodes or {})
         with open(output_json_path, 'w', encoding='utf-8') as f:
-            json.dump(self.extracted_texts, f, indent=4, ensure_ascii=False)
+            json.dump(extracted_data, f, indent=4, ensure_ascii=False)
     
     def add_cer_metric(self):
         if not self.gt_texts:
@@ -37,14 +42,44 @@ class ResultStorage:
         self.metrics['cer'] = formatted_result 
         return
     
+    def add_barcode_reading_accuracy_metric(self):
+        if not self.extracted_barcodes:
+            print("No extracted barcodes to evaluate.")
+            return
+        correct_count = 0
+        total_count = len(self.extracted_barcodes)
+        for roi_name, barcode_text in self.extracted_barcodes.items():
+            if roi_name in self.gt_texts and barcode_text == self.gt_texts[roi_name]:
+                correct_count += 1
+        accuracy = correct_count / total_count if total_count > 0 else 0
+        formatted_accuracy = f"Correctly read {correct_count}/{total_count}; Accuracy:({accuracy*100:.1f}%)"
+        self.metrics['barcode_reading_accuracy'] = formatted_accuracy
+        return
+    
     def add_extracted_texts_to_summary(self):
         self.summary_text += "\n\nExtracted Texts:\n"
         for roi_name, text in self.extracted_texts.items():
             self.summary_text += f"--- Region: {roi_name} ---\n"
             self.summary_text += text + "\n\n"
+        self.summary_text += "\n\nExtracted Barcodes:\n"
+        for roi_name, text in self.extracted_barcodes.items():
+            self.summary_text += f"--- Barcode: {roi_name} ---\n"
+            self.summary_text += text + "\n\n"
     
     def add_metric_info_to_summary(self):
-        lines = ["OCR results metric summary:"]
+        lines = ["Metric summary:"]
         for metric_name, metric_value in self.metrics.items():
             lines.append(f"{metric_name}: {metric_value}")
         self.summary_text = "\n".join(lines)
+
+    def generate_summary(self, summary_name: str, output_dir: str, verbose: bool = False, json_output: bool = True):
+        print(f"Generating summary for {summary_name}...")
+        self.add_cer_metric()
+        self.add_barcode_reading_accuracy_metric()
+        self.add_metric_info_to_summary()
+        if verbose:
+            self.add_extracted_texts_to_summary()
+        self.save_summary_to_txt(output_txt_path=f"{output_dir}/{summary_name}.txt")
+        if json_output:
+            self.save_extracted_texts_to_json(output_json_path=f"{output_dir}/{summary_name}.json")
+    
